14729e8e6e5f4b5a9c57e970318ece2fede6d402
Prateek Tripathy <tripathy.prateek@gmail.com>
2020-05-06 01:41:25 +0530

    May 5 solution added

---
+++ Leetcode May Practise/May5_first_unique_character_in_a_string.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int firstUniqChar(string s)
+    {
+        unordered_map<char, int> map;
+        for (char ch : s)
+        {
+            map[ch]++;
+        }
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (map[s[i]] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
