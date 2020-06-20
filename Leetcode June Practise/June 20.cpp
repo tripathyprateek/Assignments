8fd22643e736790c088b10367c60a03dd8e32baf
Prateek Tripathy <tripathy.prateek@gmail.com>
2020-06-20 16:36:56 +0530

    June 20 solution added

---
+++ Leetcode June Practise/June 20.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s = "";
+        for(int i = 1; i <= n; i++) 
+            s.push_back(i + '0');
+        while(--k) 
+            next_permutation(s.begin(), s.end());
+        return s;
+    }
+};
